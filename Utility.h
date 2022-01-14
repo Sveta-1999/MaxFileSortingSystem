500387b4330bb5bc5c48999878aadb15805d22f1
Sveta-1999 <79526438+Sveta-1999@users.noreply.github.com>
2022-01-14 10:45:06 +0400

    Add files via upload

---
+++ Utility.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#ifndef UTILITI_H
+#define UTILITI_H
+
+namespace Util
+{
+    const std::string unicode{"800"} ;
+    const std::string fileName{"MaxFile.txt"} ;
+    const size_t maxCount{10000000} ;
+}
+
+#endif //UTILITY_H
